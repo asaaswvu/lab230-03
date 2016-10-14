35b3e0c6dc16c0c5791140e9f4429b7f6245e23a
Jacob Alexander Jesionowski <jajesionowski@ubuntu.statler.wvu.edu>
2016-10-14 15:02:11 -0400

    I added the button.

--- NameGUI.java
+++ NameGUI.java
@@ -21,6 +21,7 @@ class NameGUI extends JFrame implements ActionListener{
         JButton btnSample = new JButton("Sample Button");
         JButton btnQuit = new JButton("Quit");
         JButton btnSwap = new JButton("Swap");
+	JButton btnName = new JButton("Jake");
         
         //create sample textboxes
         txtWord1 = new JTextField(15);
@@ -36,12 +37,15 @@ class NameGUI extends JFrame implements ActionListener{
         btnQuit.addActionListener(this);
         btnSwap.setActionCommand("swap");  
         btnSwap.addActionListener(this);
+	btnName.setActionCommand("name");
+	btnName.addActionListener(this);
  
         //Add components to proper panels
         panelTop.add(btnSample);
         panelTop.add(btnQuit);
         
         //panelMiddle.add(btnYourButton);
+	panelMiddle.add(btnName);
 
         panelBottom.add(lblSwap);
         panelBottom.add(btnSwap);
@@ -122,7 +126,8 @@ class NameGUI extends JFrame implements ActionListener{
             case "help" :
                 JOptionPane.showMessageDialog(this,"There is no help for you.","Sorry",JOptionPane.WARNING_MESSAGE);
                 break;
-                
+            case "name":
+		JOptionPane.showMessageDialog(this,"Jake Jesionowski", "My Name", JOptionPane.PLAIN_MESSAGE); 
         }
     }
     
