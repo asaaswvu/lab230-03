0384f92f0ac5f4743e9e679fab9d7718e123c1e0
Adly  Noore <anoore1@ubuntu.statler.wvu.edu>
2016-10-14 15:35:34 -0400

    Noore

--- NameGUI.java
+++ NameGUI.java
@@ -21,6 +21,7 @@ class NameGUI extends JFrame implements ActionListener{
         JButton btnSample = new JButton("Sample Button");
         JButton btnQuit = new JButton("Quit");
         JButton btnSwap = new JButton("Swap");
+        JButton btnName = new JButton("Name");
         
         //create sample textboxes
         txtWord1 = new JTextField(15);
@@ -36,17 +37,19 @@ class NameGUI extends JFrame implements ActionListener{
         btnQuit.addActionListener(this);
         btnSwap.setActionCommand("swap");  
         btnSwap.addActionListener(this);
+        btnName.setActionCommand("name");  
+        btnName.addActionListener(this);
  
         //Add components to proper panels
         panelTop.add(btnSample);
         panelTop.add(btnQuit);
         
         //panelMiddle.add(btnYourButton);
-
         panelBottom.add(lblSwap);
         panelBottom.add(btnSwap);
         panelBottom.add(txtWord1);
         panelBottom.add(txtWord2);
+        panelMiddle.add(btnName);
         
         //Add individual panels to panelMain, applies to BORDERLAYOUT only
         //PAGE_START is top of screen
@@ -122,7 +125,9 @@ class NameGUI extends JFrame implements ActionListener{
             case "help" :
                 JOptionPane.showMessageDialog(this,"There is no help for you.","Sorry",JOptionPane.WARNING_MESSAGE);
                 break;
-                
+            case "name" :
+                JOptionPane.showMessageDialog(this,"Noore","Programmer's Name",JOptionPane.PLAIN_MESSAGE);
+                break;
         }
     }
     
